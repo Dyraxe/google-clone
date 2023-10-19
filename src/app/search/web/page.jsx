6a855f1d92627288c239b6ed028bc81e6a5da9f2
Paulo Amorim <phdja25@gmail.com>
2023-10-19 19:27:56 -0300

    fetch data from google search API

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,16 @@
-function WebSearchPage() {
-  return <div>web PAGE</div>;
+async function WebSearchPage({ searchParams: { searchTerm } }) {
+  const res =
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API}&cx=${process.env.GOOGLE_SEARCH_CONTEXT_API}&q=${searchTerm}
+`);
+  const data = await res.json();
+  const results = data.items;
+  return (
+    <>
+      {results.map((result) => (
+        <p key={result.cacheId}>{result.title}</p>
+      ))}
+    </>
+  );
 }
 
 export default WebSearchPage;
